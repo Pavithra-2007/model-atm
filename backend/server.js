38ada3d8b0fdba54d5d4f33c577e3f31a5ab4ff5
Pavithra-2007 <pavithram.24it@kongu.edu>
2026-06-17 10:36:41 +0530

    Configure backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/atm", require("./routes/atmRoutes"));
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
 
@@ -20,8 +22,5 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
-app.use("/api/atm", require("./routes/atmRoutes"));
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server Running");
-});
+module.exports = app;
