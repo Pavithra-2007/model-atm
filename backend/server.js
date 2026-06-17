4428d8b123ea894b4016783d91932eb08010939d
Pavithra-2007 <pavithram.24it@kongu.edu>
2026-06-17 11:39:51 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://model-atm.vercel.app/",
+}));
 app.use(express.json());
 
 app.use("/api/atm", require("./routes/atmRoutes"));
